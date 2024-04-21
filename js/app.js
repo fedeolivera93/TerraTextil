6d90574177c18727696d2f2fc1a72557e9561f5f
fedeolivera93 <141982362+fedeolivera93@users.noreply.github.com>
2024-04-21 20:30:05 -0300

    Subiendo TerraTextil

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// nav toggle - select button and links
+
+const navToggle = document.querySelector("#navToggle")
+const nav = document.querySelector("#nav-links")
+
+// add event listener
+
+navToggle.addEventListener("click", () => {
+    nav.classList.toggle('nav-open')
+})
+
