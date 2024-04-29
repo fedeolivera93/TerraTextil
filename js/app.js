d0e1bac907bacca64f9369b9779eea703e148397
fedeolivera93 <fedeolivera93@gmail.com>
2024-04-29 00:03:51 -0300

    updateFAQSection
    Diseño de la sección FAQ

--- js/app.js
+++ js/app.js
@@ -9,3 +9,21 @@ navToggle.addEventListener("click", () => {
     nav.classList.toggle('nav-open')
 })
 
+// ------------ FAQ -----------
+
+// select faqs and answers
+const faqs = document.querySelectorAll('.faqs');
+const answers = document.querySelectorAll('.answer');
+
+// add event listener for click 
+
+
+
+
+
+faqs.forEach((faq, index) => {
+    faq.addEventListener('click', () => {
+        answers[index].classList.toggle('active');
+        faq.classList.toggle('active');
+    });
+});
