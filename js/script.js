f34bfa6dc33f08a34d8e66d5b727edfc462bb2bf
fedeolivera93 <fedeolivera93@gmail.com>
2024-05-12 18:28:29 -0300

    updateForm

--- js/script.js
+++ js/script.js
@@ -27,3 +27,50 @@ faqs.forEach((faq, index) => {
         faq.classList.toggle('active');
     });
 });
+
+
+//esto es del formulario con la validación
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+  
+    // Validación del nombre
+    var name = document.getElementById('name').value;
+    if(name == ""){
+      alert('Por favor, ingresa tu nombre y apellido.');
+      return false;
+    }
+  
+    // Validación del correo electrónico
+    var email = document.getElementById('email').value;
+    if(email == ""){
+      alert('Por favor, ingresa tu correo electrónico.');
+      return false;
+    } else if(!/^[^@]+@[^@]+\.[a-zA-Z]{2,}$/.test(email)){
+      alert('Por favor, ingresa un correo electrónico válido.');
+      return false;
+    }
+    // Validación del número de teléfono
+  var numero = document.getElementById('numero').value;
+  if(numero == ""){
+    alert('Por favor, ingresa tu número de teléfono.');
+    return false;
+  } else if(!/^\d+$/.test(numero)){
+    alert('El número de teléfono solo debe contener dígitos.');
+    return false;
+  }
+    // Validación del asunto
+    var subject = document.getElementById('subject').value;
+    if(subject == ""){
+      alert('Por favor, selecciona un asunto.');
+      return false;
+    }
+  
+    // Validación del mensaje
+    var message = document.getElementById('message').value;
+    if(message == ""){
+      alert('Por favor, ingresa tu mensaje.');
+      return false;
+    }
+  
+    alert('Formulario enviado con éxito!');
+  });
