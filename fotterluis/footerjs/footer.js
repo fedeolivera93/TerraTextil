1fbe37024c4e2a5bebbc492dcaa5dc591d165d79
fedeolivera93 <fedeolivera93@gmail.com>
2024-05-22 17:45:20 -0300

    updateFinal

--- fotterluis/footerjs/footer.js
+++
@@ -1,7 +0,0 @@
-var links = document.querySelectorAll('.footer a');
-links.forEach(function(link) {
-    link.addEventListener('click', function(e) {
-        e.preventDefault();
-        window.open(this.href, '_blank');
-    });
-});
